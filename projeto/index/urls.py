5d094ae09d8cbe6d50577a3271a07f6775a75df0
Joel <joelvieirasouza@gmail.com>
2024-01-19 15:14:30 -0300

    Update urls.py

--- projeto/index/urls.py
+++ projeto/index/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('usuario/', usuario_view.usuarios, name='usuario'),
     path('colaborador/', colaborador_view.colaborador, name='colaborador'),
     path('reuniao/', reuniao_view.reuniao, name='reuniao'),
-    path('criar_conta/', usuario_view.cadastrar_login, name='criar_conta'),
+    
+    path('accounts/signup/', usuario_view.cadastrar_login, name='accounts/signup'),
     path('accounts/login/', usuario_view.login, name='accounts/login'),
     path('accounts/logout/', usuario_view.logout, name='accounts/logout')
 ]
