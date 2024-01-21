00bca1eb96235f107045b64a29e5254041dc04d6
Joel <joelvieirasouza@gmail.com>
2024-01-20 23:06:50 -0300

    Update colaborador_view.py

--- projeto/index/views/colaborador_view.py
+++ projeto/index/views/colaborador_view.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+from index.models import Colaboradores, ColaboradoresForm
 
 @login_required
 def colaborador(request):
-    return render(request, 'colaborador.html')
+    if request.method == 'GET':
+        colaboradores = Colaboradores.objects.all()
+        return render(request, 'colaborador.html', {'colaboradores': colaboradores})
+
+    elif request.method == 'POST':
+        form = ColaboradoresForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('colaborador'))
+        else:
+            # Crie um formulário vazio (ou preenchido com os dados existentes do usuário)
+            form = ColaboradoresForm()
+            
+        return render(request, 'colaborador.html', {'form': form})
