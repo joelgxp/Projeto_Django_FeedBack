e9786425bff0dfb96646caa6d964588e9dee4d72
Joel <joelvieirasouza@gmail.com>
2024-01-20 23:06:57 -0300

    Update models.py

--- projeto/index/models.py
+++ projeto/index/models.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import models
 
+
 class Usuarios(models.Model):
     TIPO_CHOICES = (
         ("Líder", "Lider"),
@@ -27,12 +28,45 @@ class UsuarioForm(forms.ModelForm):
         'tipo': forms.Select(choices=Usuarios.TIPO_CHOICES),
     }
         
-class Reunioes(models.Model):
-    id_lider = models.CharField(max_length=255)
-    id_colaborador = models.CharField(max_length=255)
-    data = models.DateField()
+
+class Lideres(models.Model):   
+    nome = models.CharField(max_length=255)
+    departamento = models.CharField(max_length=255)
+    email = models.EmailField()
+    tipo = models.CharField(default='Líder', max_length=20)
+    senha = models.CharField(max_length=255)
     ativo = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.id_lider} - {self.id_colaborador} - {self.data}"
+        return f"{self.nome} - {self.email} - {self.departamento}"
     
+class LideresForm(forms.ModelForm):
+    class Meta:
+        model = Lideres
+        fields = ['nome', 'departamento', 'email', 'senha']
+        
+    widgets = {
+        'senha': forms.PasswordInput(),
+    }
+        
+
+class Colaboradores(models.Model):   
+    nome = models.CharField(max_length=255)
+    departamento = models.CharField(max_length=255)
+    email = models.EmailField()
+    tipo = models.CharField(default='Colaborador', max_length=20)
+    senha = models.CharField(max_length=255)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.nome} - {self.email} - {self.departamento}"
+    
+class ColaboradoresForm(forms.ModelForm):
+    class Meta:
+        model = Colaboradores
+        fields = ['nome', 'departamento', 'email', 'senha']
+        
+    widgets = {
+        'senha': forms.PasswordInput(),
+    }
+        
