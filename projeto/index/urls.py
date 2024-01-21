0c43fb32da703069a8d85cfd12558cc618f48734
Joel <joelvieirasouza@gmail.com>
2024-01-21 00:25:13 -0300

    Update urls.py

--- projeto/index/urls.py
+++ projeto/index/urls.py
@@ -3,12 +3,11 @@ from .views import index_view, usuario_view, colaborador_view, lider_view
 
 urlpatterns = [
     path('', index_view.index, name='index'),
-    path('usuario/', usuario_view.usuarios, name='usuario'),
     
     path('lider/', lider_view.lider, name='lider'),
     path('colaborador/', colaborador_view.colaborador, name='colaborador'),
     
-    path('accounts/signup/', usuario_view.cadastrar_login, name='accounts/signup'),
-    path('accounts/login/', usuario_view.login, name='accounts/login'),
+    path('accounts/signup/', usuario_view.signup, name='accounts/signup'),
+    path('accounts/signin/', usuario_view.signin, name='accounts/signin'),
     path('accounts/logout/', usuario_view.logout, name='accounts/logout')
 ]
