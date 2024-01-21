c201b1a184939d45bcbc397edf443a3254e9a29f
Joel <joelvieirasouza@gmail.com>
2024-01-20 23:06:18 -0300

    Update urls.py

--- projeto/index/urls.py
+++ projeto/index/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index_view, usuario_view, reuniao_view, colaborador_view
+from .views import index_view, usuario_view, colaborador_view, lider_view
 
 urlpatterns = [
     path('', index_view.index, name='index'),
     path('usuario/', usuario_view.usuarios, name='usuario'),
+    
+    path('lider/', lider_view.lider, name='lider'),
     path('colaborador/', colaborador_view.colaborador, name='colaborador'),
-    path('reuniao/', reuniao_view.reuniao, name='reuniao'),
     
     path('accounts/signup/', usuario_view.cadastrar_login, name='accounts/signup'),
     path('accounts/login/', usuario_view.login, name='accounts/login'),
