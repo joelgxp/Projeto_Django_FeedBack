24ec0a9c6ac5e563d3e6a7aa256bfee89434b2e5
Joel <joelvieirasouza@gmail.com>
2024-01-21 00:25:21 -0300

    Update models.py

--- projeto/index/models.py
+++ projeto/index/models.py
@@ -1,41 +1,16 @@
 from django import forms
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-
-class Usuarios(models.Model):
-    TIPO_CHOICES = (
-        ("Líder", "Lider"),
-        ("Colaborador", "Colaborador")
-    )
-    
+class Lideres(AbstractUser):   
     nome = models.CharField(max_length=255)
     departamento = models.CharField(max_length=255)
-    email = models.EmailField()
-    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES, blank=False, null=False)
-    senha = models.CharField(max_length=255)
-    ativo = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.nome} - {self.email} - {self.departamento} - {self.tipo}"
-    
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = Usuarios
-        fields = ['nome', 'departamento', 'email', 'tipo', 'senha']
-        
-    widgets = {
-        'senha': forms.PasswordInput(),
-        'tipo': forms.Select(choices=Usuarios.TIPO_CHOICES),
-    }
-        
-
-class Lideres(models.Model):   
-    nome = models.CharField(max_length=255)
-    departamento = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     tipo = models.CharField(default='Líder', max_length=20)
-    senha = models.CharField(max_length=255)
     ativo = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'password', 'nome', 'departamento', 'tipo']
 
     def __str__(self):
         return f"{self.nome} - {self.email} - {self.departamento}"
@@ -43,10 +18,10 @@ class Lideres(models.Model):
 class LideresForm(forms.ModelForm):
     class Meta:
         model = Lideres
-        fields = ['nome', 'departamento', 'email', 'senha']
+        fields = ['nome', 'departamento', 'email', 'password']
         
     widgets = {
-        'senha': forms.PasswordInput(),
+        'password': forms.PasswordInput(),
     }
         
 
