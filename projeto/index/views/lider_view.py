ea9bbc9ce0ba3151cec76b743d7354e2e9008325
Joel <joelvieirasouza@gmail.com>
2024-01-22 17:06:41 -0300

    update lider

--- projeto/index/views/lider_view.py
+++ projeto/index/views/lider_view.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from rolepermissions.roles import assign_role
 
-from index.models import Lideres, LideresForm
 
-@login_required
+from index.models import Lider, LiderForm
+
+@login_required(login_url='accounts/signin')
 def lider(request):
     if request.method == 'GET':
-        usuarios = Lideres.objects.all()
-        return render(request, 'usuario.html', {'usuarios': usuarios})
+        usuarios = Lider.objects.all()
+        return render(request, 'usuario.html', {'usuarios': usuarios, 'usuario': request.user.username})
     
     elif request.method == 'POST':
-        form = LideresForm(request.POST)
+        form = LiderForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(reverse('usuario'))
+            return redirect(reverse('lider'))
         else:
             # Crie um formulário vazio (ou preenchido com os dados existentes do líder)
-            form = LideresForm()
+            form = LiderForm()
             
         return render(request, 'usuario.html', {'form': form})
+  
