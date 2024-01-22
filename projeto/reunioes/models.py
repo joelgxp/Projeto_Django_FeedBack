78526b3b1b5a6877652fed64916b6dd3f90a7f66
Joel <joelvieirasouza@gmail.com>
2024-01-22 17:07:53 -0300

    update reuniiao

--- projeto/reunioes/models.py
+++ projeto/reunioes/models.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.db import models
-from index.models import Lideres, Colaboradores
+from index.models import Lider, Colaborador
 
 class Reuniao(models.Model):
-    id_lider = models.ForeignKey(Lideres, on_delete=models.DO_NOTHING)
-    id_colaborador = models.ForeignKey(Colaboradores, on_delete=models.DO_NOTHING)
+    id_lider = models.ForeignKey(Lider, on_delete=models.DO_NOTHING)
+    id_colaborador = models.ForeignKey(Colaborador, on_delete=models.DO_NOTHING)
     data = models.DateField()
     ativo = models.BooleanField(default=True)
 
@@ -15,3 +15,16 @@ class ReuniaoForm(forms.ModelForm):
     class Meta:
         model = Reuniao
         fields = ['id_lider', 'id_colaborador', 'data']
+        widgets = {
+            'data': forms.DateInput(attrs={'type': 'date'})
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ReuniaoForm, self).__init__(*args, **kwargs)
+        
+        # Adicionando as escolhas para o campo 'id_lider'
+        self.fields['id_lider'].queryset = Lider.objects.filter(ativo=True)
+
+        # Adicionando as escolhas para o campo 'id_colaborador'
+        self.fields['id_colaborador'].queryset = Colaborador.objects.filter(ativo=True)
+ 
--- projeto/reunioes/views/reuniao_view.py
+++ projeto/reunioes/views/reuniao_view.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.db.models import Q
 
 from ..models import Reuniao, ReuniaoForm
 
-@login_required
+@login_required(login_url='accounts/signin')
 def reuniao(request):
     if request.method == 'GET':
-        reunioes = Reuniao.objects.all()
-        return render(request, 'reuniao.html', {'reunioes': reunioes})
+        reunioes = Reuniao.objects.filter(Q(id_lider_id__username=request.user.username) | Q(id_colaborador_id__nome=request.user.username))
+
+        return render(request, 'reuniao.html', {'reunioes': reunioes, 'usuario': request.user.username})
+    
     elif request.method == 'POST':
         form = ReuniaoForm(request.POST)
         if form.is_valid():
-            form.save()
+            try:
+                form.save()
+            except Exception as e:
+                print(f"erro ao salvar: {e}")    
             return redirect(reverse('reunioes'))
         else:
             form = ReuniaoForm()
